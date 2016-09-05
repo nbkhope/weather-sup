113d5d0fba0e8de1423347867dca19ac2d16daf0
nbkhope <ryumemaru@yahoo.com>
2016-09-05 15:32:08 -0700

    Define WeatherReducer (concats new data to the state)

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,7 +1,17 @@
-export default function(state = null, action) {
-  console.log('Action received', action);
+import { FETCH_WEATHER } from '../actions/index';
 
-  
+export default function(state = [], action) {
+  console.log('Action received', action);
+  // each element in the state [] will hold a city
+
+  switch (action.type) {
+    case FETCH_WEATHER:
+      // you cannot mutate the state, so use concat
+      return state.concat([ action.payload.data ]);
+      // equivalent, using spread operator:
+      //return [ action.payload.data, ...state ];
+      break;
+  }
 
   return state;
 }
