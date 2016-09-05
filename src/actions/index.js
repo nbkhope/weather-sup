cb5b856afa99fbef58a83dd5d8ae63b563abc8a4
nbkhope <ryumemaru@yahoo.com>
2016-09-05 15:03:13 -0700

    Define actions index file with Action Creator to fetch weather data

---
+++ src/actions/index.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+const API_KEY = 'edab3563100a287807a81ad7e3b73d78';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+// By convention, use constants to hold action type
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+// Action Creator
+export function fetchWeather(city) {
+  // Add query for specific city & country
+  const url = `ROOT_URL&q=${city},us`;
+  // Make 'Ajax' call using axios (it returns a promise)
+  const request = axios.get(url);
+
+  // Return an action
+  // (an object that has a type)
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
