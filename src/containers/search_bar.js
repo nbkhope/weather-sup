a600f2ef60a795166d793ba2660da38239904982
nbkhope <ryumemaru@yahoo.com>
2016-09-05 14:10:41 -0700

    Add SearchBar container component

---
+++ src/containers/search_bar.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+
+export default
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    // Initialize the component state
+    this.state = {
+      term: ''
+    };
+
+    // Bind the correct context for the *this* keyword
+    this.onInputChange = this.onInputChange.bind(this);
+  }
+
+  onInputChange(event) {
+    this.setState({ term: event.target.value });
+  }
+
+  render() {
+    return (
+      <form className="input-group">
+        <input
+          placeholder="Check the five-day forecast for a city"
+          className="form-control"
+          value={this.state.term}
+          onChange={this.onInputChange}
+        />
+        <span className="input-group-btn">
+          <button type="submit" className="btn btn-secondary">Check</button>
+        </span>
+      </form>
+    );
+  }
+}
