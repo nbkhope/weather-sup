3217c88a344b2ecbda3b34e146ccb24288f0239d
nbkhope <ryumemaru@yahoo.com>
2016-09-06 13:33:25 -0700

    Make table rows work with city name and its temperature graph

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,7 +1,32 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Sparklines, SparklinesLine } from 'react-sparklines';
 
 class WeatherList extends Component {
+  renderWeather(cityData) {
+    console.log(cityData);
+    const name = cityData.city.name;
+    // Map temperature values inside an array
+    const temps = cityData.list.map(weather => weather.main.temp);
+    // Map humidity to an array of values
+    const humidities = cityData.list.map(weather => weather.main.humidity);
+    // Map pressure values
+    const pressures = cityData.list.map(weather => weather.main.pressure);
+
+    return (
+      <tr key={name}>
+        <td>{name}</td>
+        <td>
+          <Sparklines width={180} height={120} data={temps}>
+            <SparklinesLine color="red" />
+          </Sparklines>
+        </td>
+        <td>{cityData.list[0].main.humidity}</td>
+        <td>{cityData.list[0].main.pressure}</td>
+      </tr>
+    );
+  }
+
   render() {
     return (
       <table className="table table-hover">
@@ -14,9 +39,7 @@ class WeatherList extends Component {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            
-          </tr>
+          {this.props.weather.map(this.renderWeather)}
         </tbody>
       </table>
     );
